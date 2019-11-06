c41db032115ba5495fb0e01e47e7102da8708d9b
Scincy <uji3210@gmail.com>
2019-11-06 16:58:59 +0900

    더하기 더하기! 수많은 더하기git add oss/main.py! 이제 그만git add oss/main.py!

--- oss/main.py
+++ oss/main.py
@@ -16,7 +16,9 @@ def ping():
 
 @app.route('/add/<num1>/<num2>')
 def add(num1, num2):
-    raise NotImplementedError("Not Implemented")
+    result=int(num1)+int(num2)
+    return str(result)
+    #raise NotImplementedError("Not Implemented")
 
 @app.route('/minus/<num1>/<num2>')
 def minus(num1, num2):
